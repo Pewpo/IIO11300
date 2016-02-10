7c34df579c8885afc82634e0addf42766c038e2a
Pewpo <samulir@windowslive.com>
2016-02-10 12:28:24 +0200

    Jatkettiin tehtävää 04

---
+++ IIO11300Vktehtavat/Tehtava 04/BLPelaaja.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava_04
+{
+    class BLPelaaja
+    {
+        private  string etunimi;
+        private string sukunimi;
+        private readonly string kokonimi;
+        private readonly string esitysnimi;
+        private string seura;
+        private double siirtohinta;
+        //konstruktorit
+      public  BLPelaaja(string enimi, string snimi, double shinta, string seura)
+        {
+            this.etunimi = enimi;
+            this.sukunimi = snimi;
+            this.kokonimi = enimi +" "+snimi;
+            this.esitysnimi = enimi + " " + snimi + "," + seura;
+            this.seura = seura;
+            this.siirtohinta = shinta;
+        }
+        public BLPelaaja() {
+            this.etunimi = "";
+            this.sukunimi = "";
+            this.kokonimi = "";
+            this.esitysnimi = "";
+            this.seura = "";
+            this.siirtohinta = 0;
+        }
+        //desktuktori
+         ~BLPelaaja()  
+        {
+            this.etunimi = null;
+            this.sukunimi = null;
+            this.seura = null;
+            this.siirtohinta = 0;
+        }
+    public  string  GetPelaajatiedot()
+        {
+            return this.esitysnimi + "<br>";
+        }
+
+
+    }
+}
--- IIO11300Vktehtavat/Tehtava 04/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava 04/MainWindow.xaml.cs
@@ -20,9 +20,44 @@ namespace Tehtava_04
     /// </summary>
     public partial class MainWindow : Window
     {
+       
+        List<BLPelaaja> pelaajat;
+        
         public MainWindow()
         {
             InitializeComponent();
+            pelaajat = new List<BLPelaaja>();
+
+        }
+
+        private void btnUusi_Click(object sender, RoutedEventArgs e)
+        {           
+            try
+            {   
+                if (txtboxEnimi.Text != null && txtboxSnimi.Text != null && txtboxHinta.Text != null || txtboxHinta.Text != null && cboxSeura.Text != null)
+                {
+                    bool apu = true;
+                    BLPelaaja i = new BLPelaaja(txtboxEnimi.Text, txtboxSnimi.Text, double.Parse(txtboxHinta.Text), cboxSeura.Text);
+                    foreach (BLPelaaja prime in pelaajat) // Loop through List with foreach.
+                    {
+                        if(i == prime) { apu = false; }
+                    }
+                    if (apu == true) {
+                        pelaajat.Add(i);
+                        applyChanges();
+                    }
+                }               
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Täytä kaikki kentät");
+            }
+        }
+        private void applyChanges()
+        {
+            listBox.ItemsSource = null;
+            listBox.ItemsSource = pelaajat;     
         }
     }
 }
