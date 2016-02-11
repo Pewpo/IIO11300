3c9961d49f099af7261900317f70b0c8a2991f59
Pewpo <samulir@windowslive.com>
2016-02-11 11:09:29 +0200

    <Tuntitehtava>  Tuntitehtäviä 4 (Toinen on kesken)

---
+++ Tuntiteht/AudioMediaplayer/H4TyontekijatConsole/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace H4TyontekijatConsole
+{
+    class Program
+    {
+        static void ReadWorkersFromXML(string filu)
+        {
+            try
+            {
+                //tutkitaan onko tiedostoa olemassa
+                if (System.IO.File.Exists(filu))
+                {
+                    //luetaan XML-tiedosto XmlDocument-olioon
+                    XmlDocument xmldoc = new XmlDocument();
+                    xmldoc.Load(filu);
+                    //haetaan kaikki työntekijä-elementit XPathilla
+                    XmlNodeList xnl = xmldoc.SelectNodes("/tyontekijat/tyontekija");
+                    XmlNode xn; //tämä edustaa yksittäistä noodia
+                    XmlNodeList  xnl2;
+                    //loopitetaan nodelista läpi
+                    for(int i = 0; i < xnl.Count; i++)
+                    {
+                        //näytetään käyttäjälle noodien sisältö
+                        xn = xnl.Item(i);
+                        Console.WriteLine(xn.InnerText);
+                        xnl2 = xn.ChildNodes;
+                        for(int j = 0; j < xnl2.Count; j++)
+                        {
+                            Console.WriteLine(xnl2.Item(j).InnerText);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        static void CalculateSalarySumFromXML(string filu)
+        {
+            try
+            {
+                //tutkitaan onko tiedostoa olemassa
+                if (System.IO.File.Exists(filu))
+                {
+                    //luetaan XML-tiedosto XmlDocument-olioon
+                    XmlDocument xmldoc = new XmlDocument();
+                    xmldoc.Load(filu);
+                    //haetaan kaikkien vakituisten työntekijöiden palkka-elementit XPathilla
+                    XmlNodeList xnl = xmldoc.SelectNodes("/tyontekijat/tyontekija[tyosuhde = 'vakituinen']/palkka");
+                    //loopitetaan nodelista läpi
+                    int yht = 0;
+                    for (int i = 0; i < xnl.Count; i++)
+                    {
+                        yht += Convert.ToInt32(xnl.Item(i).InnerText);
+                    }
+                    Console.WriteLine(string.Format(
+                        "Vakituisia on {0} ja heidän palkat yhteensä {1}",xnl.Count, yht));
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                //    ReadWorkersFromXML("d:\\työntekijät2016.xml");
+                CalculateSalarySumFromXML("d:\\työntekijät2016.xml");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Tuntiteht/AudioMediaplayer/H4TyontekijatWPF/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace H4TyontekijatWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnReadXML_Click(object sender, RoutedEventArgs e)
+        {
+            //luetaan XML -tiedostosta tyontekijaä-elementit ja sidotaan ne tähän DataGridiin
+            try
+            {
+                string filu = "D:\\Työntekijät2016.xml"; //tiedostonimi kovakoodattu, välttäkää kovakoodausta!
+                XElement xe = XElement.Load(filu);
+                xd = new XmlDocument();
+                xd.Load(filu);
+                tyontekijat = xd.SelectNodes("/tyontekij");
+                dgData.DataContext = xe.Elements("tyontekija");
+                tbMessage.Text = string.Format("Vakituisia työntekijöitä {0} ja palkat {1}", CountWorkers("vakituinen"), CalculateSalarySum());
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        private int CountWorkers()
+        {
+            int lkm = 0;
+            lkm = tyontekijat.Count;
+            return lkm;
+        }
+        private int CountWorkers(string tyosuhde)
+        {
+            int lkm = 0;
+            //lasketaan LINQ-Kyselyllä tietyntyyppiset työntekijät
+            var temp = from ele
+                       in xe.Elements()
+                       where ele.Element("tyosuhde").Value == tyosuhde
+                       select ele.Element("sukunimi");
+            lkm = temp.Count();
+            return lkm;
+        }
+        private decimal CalculateSalarySum()
+        {
+            decimal sum = 0;
+            XmlNode xn;
+            for(int i = 0; i < tyontekijat.Count; i++)
+            {
+                xn = tyontekijat.Item(i);
+                sum += decimal.Parse(xn.LastChild.InnerText);
+            }
+            return sum;
+        }
+        private decimal CalculateSalarySum(string tyosuhde)
+        {
+            decimal sum = 0;
+            var palkat = from ele
+                         in xe.Elements()
+                         where ele.Element("tyoshude").Value == tyosuhde
+                         select ele.Element("palkka").Value;
+            foreach(var item in palkat)
+            {
+                System.Diagnostics.Debug.Print(item.ToString());
+                sum += decimal.Parse(item);
+            }
+                return sum;
+        }
+    }
+}
