5433209faba2c509c5747ccdaa54b2b8244f5e7f
pewpo <samulir@windowslive.com>
2016-02-23 13:29:59 +0200

    Jatketttiin tehtävää 06a

--- IIO11300Vktehtavat/06a/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/06a/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.IO;
 
 namespace _06a
 {
@@ -20,14 +23,20 @@ namespace _06a
     /// </summary>
     public partial class MainWindow : Window
     {
+        XmlDataProvider xmldp;
         public MainWindow()
         {
             InitializeComponent();
+            //haetaan tiedoston sijainti app.configista
+            string vineFileLocation;
+            vineFileLocation = ConfigurationManager.AppSettings.Get("vineFile");
+            xmldp.Source = new Uri("vineFileLocation");
         }
 
         private void btnHaeViinit_Click(object sender, RoutedEventArgs e)
         {
-  
+
+
         }
     }
 }
