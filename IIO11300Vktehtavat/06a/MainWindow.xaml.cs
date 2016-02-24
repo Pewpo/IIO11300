9c7f1995d8c07db85ba400bc48e4fda965e14871
pewpo <samulir@windowslive.com>
2016-02-24 21:19:42 +0200

    Tehtävä 06A valmis

--- IIO11300Vktehtavat/06a/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/06a/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.Configuration;
 using System.Collections.Specialized;
 using System.IO;
+using System.Xml;
 
 namespace _06a
 {
@@ -23,20 +24,72 @@ namespace _06a
     /// </summary>
     public partial class MainWindow : Window
     {
-        XmlDataProvider xmldp;
+        XmlDataProvider xmldp = new XmlDataProvider();
+        XmlDocument doc = new XmlDocument();
         public MainWindow()
         {
             InitializeComponent();
             //haetaan tiedoston sijainti app.configista
             string vineFileLocation;
-            vineFileLocation = ConfigurationManager.AppSettings.Get("vineFile");
-            xmldp.Source = new Uri("vineFileLocation");
+            try
+            {
+                vineFileLocation = ConfigurationManager.AppSettings.Get("vineFile");
+                xmldp.Source = new Uri(vineFileLocation);
+                xmldp.XPath = "/viinikellari/wine";
+                tbStatus.Text = vineFileLocation;
+                doc.Load(vineFileLocation);
+                XmlNodeList root = doc.SelectNodes("/viinikellari/wine/maa");
+                //syötetään maat comboboxiin
+                comboBox.Items.Add("Näytä Kaikki viinit");
+                foreach (XmlNode nimi in root)
+                {
+                    if (!comboBox.Items.Contains(nimi.InnerText))
+                    {
+                        comboBox.Items.Add(nimi.InnerText);
+                    }
+                }
+                comboBox.SelectedIndex = 0;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("jokin meni pieleen tiedostonhaussa tai maiden lisäämisessä comboboksiin. Korjaa virheet ja käynnistä ohjelma uudestaan");
+                Application.Current.Shutdown();
+            }
         }
 
         private void btnHaeViinit_Click(object sender, RoutedEventArgs e)
         {
-
-
+            try
+            {
+                dgNayta.Items.Clear();
+                XmlNodeList root = doc.SelectNodes("/viinikellari/wine");
+                //jos on valittu kaikki niin ajellaan foreachilla kaikki taulukkoon
+                if (comboBox.SelectedItem.ToString() == "Näytä Kaikki viinit")
+                {
+                    foreach (XmlNode nimi in root)
+                    {
+                        dgNayta.Items.Add(nimi).ToString();
+                    }
+                }
+                else
+                {//jos ei ole valittu kaikkia niin selataan mikä on valittu ajellaan foreachilla taulukkoon
+                    int i = 0;
+                    foreach (XmlNode nimi in root)
+                    {
+                        var node = nimi.SelectNodes("/viinikellari/wine/maa");
+                        if (node.Item(i).InnerText == comboBox.SelectedItem.ToString())
+                        {
+                            dgNayta.Items.Add(nimi).ToString();
+                        }
+                        i++;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
-}
+  }
+
